38f826fa96e1dfce3d80ddadab864392d91d331b
KrotVP <carlosevepereira8@gmail.com>
2022-03-28 19:28:46 -0300

    Revisao

---
+++ script.bat
@@ -0,0 +1,11 @@
+cd c:\exemplo
+echo "Criando pasta ! Aguarde"
+timeout /t 5
+mkdir outros
+echo "Pasta criada com sucesso!"
+echo "Teste de arquivo - Serratec 2022" > arquivo6.txt
+echo "Criando segunda pasta!"
+timeout /t 5
+mkdir outros-exemplos
+echo "Teste de arquivo - Serratec 2022" > arquivo7.txt
+echo "Fim do Script!"
